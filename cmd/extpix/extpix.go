e387d94aca072ab302cdc7bd243ef88e6f24e7e2
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2025-04-25 16:12:43 +0200

    extpix improvements

--- cmd/extpix/extpix.go
+++ cmd/extpix/extpix.go
@@ -11,19 +11,45 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: extpix <id> <image>\n")
+}
+
+// gno-logo.png dx=80 dy=30
+// arm.png dx=50 dy=50
+
 func main() {
 	fs := flag.NewFlagSet("extpix", flag.ContinueOnError)
+	dx := fs.Int("dx", 0, "x offset")
+	dy := fs.Int("dy", 0, "y offset")
+	// default is tom101 address
+	addr := fs.String("addr", "g1w3saysjxdlsyczysnyfd55tuvhhz5533nef8y7", "signer address")
+	// default is min gas required for empty canvas
+	gasInit := fs.Float64("gas", 3000000, "gas wanted for first tx")
+
 	if err := fs.Parse(os.Args[1:]); err != nil {
+		usage()
 		log.Fatalf("flag error: %s", err)
 	}
-	f, err := os.Open(fs.Arg(0))
+	if fs.NArg() != 2 {
+		usage()
+		log.Fatalf("invalid args")
+	}
+
+	id := fs.Arg(0)
+	if _, err := strconv.Atoi(id); err != nil {
+		usage()
+		log.Fatalf("%s is not an int", fs.Arg(0))
+	}
+	f, err := os.Open(fs.Arg(1))
 	if err != nil {
-		log.Fatalf("%s could not be opened", os.Args[1])
+		log.Fatalf("%s could not be opened", fs.Arg(1))
 	}
 	defer f.Close()
 
@@ -37,26 +63,26 @@ func main() {
 	if err != nil {
 		log.Fatalf("err read password: %v", err)
 	}
-	pwd := string(bz)
-
 	var (
-		id   = "6"
-		addr = "g1w3saysjxdlsyczysnyfd55tuvhhz5533nef8y7"
+		pwd       = string(bz)
+		gasWanted = *gasInit
 	)
 	for i, p := range pixels {
-		// if i < 387 {
-		// continue
-		// }
+		// gas-price is 0.001 but a small extra of 0.0001 is needed
+		gasFee := int(gasWanted * .0011)
+		fmt.Println(gasWanted, gasFee)
 		cmd := exec.Command("gnokey", "maketx", "call",
 			"-pkgpath", "gno.land/r/tom101/pixels",
 			"-func", "AddPixel",
 			"-args", id,
-			"-args", fmt.Sprint(p.x+80),
-			"-args", fmt.Sprint(p.y+30),
+			"-args", fmt.Sprint(p.x+*dx),
+			"-args", fmt.Sprint(p.y+*dy),
 			"-args", p.color,
 			"-insecure-password-stdin=true",
-			"-gas-fee", "3000ugnot", "-gas-wanted", "30000000", "-broadcast",
-			"-chainid", "dev", "-remote", "tcp://127.0.0.1:26657", addr,
+			"-gas-fee", fmt.Sprintf("%dugnot", gasFee),
+			"-gas-wanted", fmt.Sprintf("%.f", gasWanted),
+			"-broadcast", "-chainid", "dev", "-remote", "tcp://127.0.0.1:26657",
+			*addr,
 		)
 		cmd.Stdin = strings.NewReader(pwd + "\n")
 		out, err := cmd.CombinedOutput()
@@ -65,6 +91,9 @@ func main() {
 		}
 		fmt.Println(string(out))
 		fmt.Printf("TX %d passed\n", i)
+
+		// inc gas by 0.7%
+		gasWanted *= 1.007
 	}
 	fmt.Println(len(pixels), "tx passed")
 }

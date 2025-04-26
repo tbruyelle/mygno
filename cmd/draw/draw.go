242347b9c87daa2e7fa8a698dec606c6b899804e
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2025-04-26 18:03:25 +0200

    use strings.Builder but still alloc limit is still reach

--- cmd/draw/draw.go
+++ cmd/draw/draw.go
@@ -22,9 +22,9 @@ func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: draw <id> <image>\n")
 }
 
-// gno-logo.png (28x47) dx=80 dy=30
-// arm.png      (32x38) dx=50 dy=50
-// arm2.png     (32x38) dx=50+32+24=106 dy=50
+// gno-logo.png (28x47) dx=80           dy=30 fees=13,856,442ugnot
+// arm.png      (32x38) dx=50           dy=50 fees=28,629,712ugnot
+// arm2.png     (32x38) dx=50+32+24=106 dy=50 fees=
 
 func main() {
 	var (
@@ -32,7 +32,7 @@ func main() {
 		dx         = fs.Int("dx", 0, "x offset")
 		dy         = fs.Int("dy", 0, "y offset")
 		addr       = fs.String("addr", "g1w3saysjxdlsyczysnyfd55tuvhhz5533nef8y7", "signer address")
-		startGas   = fs.Float64("start-gas", 3500000, "gas wanted for first tx")
+		startGas   = fs.Float64("start-gas", 5_000_000, "gas wanted for first tx")
 		startPixel = fs.Int("start-pixel", 0, "send tx after specified pixel")
 	)
 
@@ -102,8 +102,8 @@ func main() {
 		fmt.Println(string(out))
 		fmt.Printf("TX %d passed, Fees consumed %dugnot\n", i, totalFee)
 
-		// inc gasWanted by 5%
-		gasWanted = gasUsed * 1.05
+		// inc gasWanted by 1%
+		gasWanted = gasUsed * 1.01
 	}
 }
 
